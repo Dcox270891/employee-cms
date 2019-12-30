d8f4c6cb04957462b1da8b89d15bf21e43905552
Dcox270891 <55593346+Dcox270891@users.noreply.github.com>
2019-12-30 12:41:46 +0000

    Got the server running and connected to the database

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 4567;
+const mysql = require(`mysql`);
+const connection = mysql.createConnection({
+    host:`localhost`,
+    user:`root`,
+    password:`test`,
+    database:`employee`,
+});
+
+connection.connect(err => {
+    console.log(err);
+});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.listen(PORT, function() {
     console.log(`Server listening on: http://localhost:${PORT}`);
