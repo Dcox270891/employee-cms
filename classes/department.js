ef3e2a3013dfd4a29f16f4ad36c2beb7bf067f78
Dcox270891 <55593346+Dcox270891@users.noreply.github.com>
2019-12-30 17:24:35 +0000

    View current working correctly

--- classes/department.js
+++ classes/department.js
@@ -1,6 +1,7 @@
 class Department {
     constructor(name){
         this.name = name;
+    }
 };
 
 module.exports = Department;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 4567;
 const mysql = require(`mysql`);
+const inquirer = require("inquirer");
+const cTable = require('console.table');
 const Employee = require(`./classes/emplyee.js`);
 const Department = require(`./classes/department.js`);
 const Role = require(`./classes/role.js`);
+const PORT = process.env.PORT || 4567;
+const prompt = inquirer.createPromptModule();
+
 const connection = mysql.createConnection({
     host:`localhost`,
     user:`root`,
@@ -12,8 +16,55 @@ const connection = mysql.createConnection({
     database:`employee`,
 });
 
-connection.connect(err => {
-    console.log(err);
+connection.connect(function (err){
+    if (err) throw err;
+    inquirer
+        .prompt([{
+            type : `list`,
+            message : `Please pick a option from the following`,
+            name : `inital_request`,
+            choices : [
+                {
+                    name : `Add a new department`
+                },
+                {
+                    name : `Add a new role`
+                },
+                {
+                    name : `Add a new employee`
+                },
+                {
+                    name : `View current departments`
+                },
+                {
+                    name : `View current roles`
+                },
+                {
+                    name : `Veiw current emlpoyees`
+                },
+                {
+                    name : `Update current emlpoyees`
+                },
+            ]
+        }])
+        .then(answer =>{
+            if(answer.inital_request === `View current departments`){
+                connection.query(`SELECT * FROM department`, (err, res)=>{
+                    if (err) throw err;
+                    console.table(res);
+                }
+            )} else if(answer.inital_request === `View current roles`){
+                connection.query(`SELECT * FROM role`, (err, res)=>{
+                    if (err) throw err;
+                    console.table(res);
+                }
+            )} else if(answer.inital_request === `View current employees`){
+                connection.query(`SELECT * FROM employee`, (err, res)=>{
+                    if (err) throw err;
+                    console.table(res);
+                }
+            )}
+        })
 });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
